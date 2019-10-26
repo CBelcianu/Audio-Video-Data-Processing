6bf1ad179be23c8f0b722ba23bd24f8b1b2ee560
Florin Pricop <pfir2383@scs.ubbcluj.ro>
2019-10-26 17:56:02 +0300

    Done

--- Lab1/AppStart.cpp
+++ Lab1/AppStart.cpp
@@ -139,16 +139,132 @@ void prettyMatrixPrint(vector<vector<float>> Matrix){
     
 }
 
+void encoder(vector<vector<float>> Y,vector<vector<float>> U,vector<vector<float>> V,vector<Block> &YC,vector<Block> &UC,vector<Block> &VC){
+    YC=divideMatrixBy8(Y);
+    vector<Block> aux=divideMatrixBy8(U);
+
+    for(Block b:aux){
+        UC.push_back(comprime(b));
+    } 
+
+    aux=divideMatrixBy8(V);
+
+    for(Block b:aux){
+        VC.push_back(comprime(b));
+    }
+
+}
+
+
+Block decomprime(Block b){
+    Block c{8};
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            c.set(i,j,b.get(ceil(i/2),ceil(j/2)));
+        }
+    }
+    return c;
+}
+
+vector<vector<float>> buildMatrixBy8(vector<Block> blocks,int width,int height){
+    vector<vector<float>> rez;
+    
+    for(int i=0; i<height; i++){
+        vector<float> aux;
+        for(int j=0; j<width; j++)
+            aux.push_back(-1);
+        rez.push_back(aux);
+    }
+
+
+    int nrBlock=0;
+    for(int i=0; i<height; i+=8){
+        for(int j=0; j<width; j+=8){
+            for(int k=i;k<i+8;k++){
+                for(int l=j;l<j+8;l++){
+                    rez[k][l]=blocks[nrBlock].get(k-i,l-j);
+                }
+            }
+            nrBlock++;
+        }
+    }
+
+    return rez;
+
+}
+
+
+void writePPM(vector<vector<float>> Y, vector<vector<float>> U, vector<vector<float>> V){
+    ofstream out("newton.ppm");
+
+    out<<"P3";
+    out<<endl;
+
+    out<<"# CREATOR: GIMP PNM Filter Version 1.1";
+    out<<endl;
+
+    out<<Y[0].size()<<" "<<Y.size();
+    out<<endl;
+
+    out<<255;
+    out<<endl;
+
+    for(int i=0;i<Y.size();i++){
+        for(int j=0;j<Y[i].size();j++){
+            float R,G,B;
+            R=Y[i][j]+1.402*(V[i][j]-128);
+            G=Y[i][j]-0.344136*(U[i][j]-128)-0.714136*(V[i][j]-128);
+            B=Y[i][j]+1.7790*(U[i][j]-128);
+
+            if (R > 255) R = 255.0;
+            if (G > 255) G = 255.0;
+            if (B > 255) B = 255.0;
+
+            if (R < 0) R = 0.0;
+            if (G < 0) G = 0.0;
+            if (B < 0) B = 0.0;
+
+            out<<(int)R<<endl<<(int)G<<endl<<(int)B<<endl;
+
+        }
+    }
+
+}
+
+
+void decoder(vector<Block> Y,vector<Block> U,vector<Block> V,int width,int height){
+    vector<Block> UD,VD;
+    for(Block b:U){
+        UD.push_back(decomprime(b));
+    }
+
+
+    for(Block b:V){
+        VD.push_back(decomprime(b));
+    }
+
+    vector<vector<float>> YM, UM, VM;
+
+    YM=buildMatrixBy8(Y,width,height);
+    UM=buildMatrixBy8(UD,width,height);
+    VM=buildMatrixBy8(VD,width,height);
+
+    writePPM(YM,UM,VM);
+    
+}
+
+
 int main(){
     vector<vector<float>> Y, U, V;
+    vector<Block> YC,UC,VC;
 
     loadMatrices(Y, U, V);
-    cout<<endl;
-    vector<Block> blocks = divideMatrixBy8(U);
-    prettyBlockPrint(blocks[0]);
-    Block test = comprime(blocks[0]);
-    cout<<endl;
-    prettyBlockPrint(test);
+    
+    cout<<"Encode...."<<endl;
+    encoder(Y,U,V,YC,UC,VC);
+    cout<<"Decode...."<<endl;
+    decoder(YC,UC,VC,Y[0].size(),Y.size());
+
 
 
     return 1;
